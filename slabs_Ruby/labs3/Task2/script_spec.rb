90597c821164fff9accfcfdada72a3db39f26c01
Anastasia545555 <100681172+Anastasia545555@users.noreply.github.com>
2022-03-02 01:21:07 +0300

    revision of tests

--- slabs_Ruby/labs3/Task2/script_spec.rb
+++ slabs_Ruby/labs3/Task2/script_spec.rb
@@ -3,6 +3,6 @@ require_relative './script'
 RSpec.describe do
   it "#script" do
     allow_any_instance_of(Kernel).to receive(:gets).and_return(18, 22, 23, 66, 17, 1)
-    expect(script).to eq ([["Andrew YA", 18]])
+    expect(script).to eq ([["Pak Yeng", 18]])
   end
 end
